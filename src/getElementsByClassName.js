d292321afaba568e84ce8fa163d0fa23a97be093
mfmehdi <mfmehdi2@gmail.com>
2019-11-19 13:55:44 +0100

    stringifyJSON done , getElementsByClassName loading..

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var arrayOfelements=[];
+  	  //var arrayBody=document.body.childNodes
+  	  var search = function(node){
+	if(node.childNodes.length === 0 && node.className === className){
+		arrayOfelements.push(node)
+	}
+
+	if(node.className === className){
+		arrayOfelements.push(node)
+	}
+
+	for(let i =0; i < node.childNodes.length; i++){
+		search(node.childNodes[i]);
+		}
+	}
+	search(document.body);
+	return arrayOfelements
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,65 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	if(Array.isArray(obj)){
+		if(obj.length === 0){
+			return "[]";
+		}
+		var result = "["
+		for (var i = 0; i < obj.length; i++) {
+			if(i=== obj.length-1){
+				result = result + stringifyJSON(obj[i])
+			}
+			else{result = result + stringifyJSON(obj[i])+',';}
+			
+		}
+		
+	//	result = result.slice(0, result.length - 1);
+		 result = result+"]";
+	} else if( typeof obj === "object" && obj!== null){
+		var result = "{"
+		
+		for(var i = 0; i < Object.keys(obj).length ; i++){
+			if(obj[Object.keys(obj)[i]] === undefined && typeof obj[Object.keys(obj)[i]] === "function"){
+				delete obj[Object.keys(obj)[i]];
+				i--
+			}
+			if(obj[Object.keys(obj)[i]] !== undefined && typeof obj[Object.keys(obj)[i]] !== "function")
+			{
+
+			 if(i===Object.keys(obj).length - 1) result = result +stringifyJSON(Object.keys(obj)[i])+":"+stringifyJSON(obj[Object.keys(obj)[i]])
+
+				else {result = result +stringifyJSON(Object.keys(obj)[i])+":"+stringifyJSON(obj[Object.keys(obj)[i]])+",";}
+			}
+	}   	
+		
+	//	result = result.slice(0, result.length - 1);
+		result = result+"}";
+	}
+	else {
+
+
+		if( typeof obj === "number" || typeof obj === "boolean" ){
+			if(isNaN(obj)){
+				return "null"
+			}
+				return obj.toString()
+		}
+		else { 
+			if(typeof obj === "string"){
+				return  '"'+ obj +'"';
+			}
+			else if(Boolean(obj) === false){
+				console.log(obj)
+				return "null";
+			}
+		}
+	}
+
+	return result;
+
+
 };
+
+var obj = {"a":{1:"n"}, b:[1,2,3,4,5]}// 
